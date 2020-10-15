3e1b7599154578960ace70fa75bf234143dd69f3
Nick Jones <nrjones8@gmail.com>
2020-10-15 09:41:20 -0400

    add hostnames from sites tracked by covidtracking.com
    original source: https://docs.google.com/spreadsheets/d/18oVRrHj3c183mHmq3m89_163yuYltLNlOmPerQ18E8w/edit#gid=1983833656
    linked to from https://github.com/COVID19Tracking/covid-tracking#urlsyaml

--- combine_all_domains_to_check.py
+++ combine_all_domains_to_check.py
@@ -22,6 +22,30 @@ def read_from_csv(source_file, column_name):
 
     return domains
 
+
+def read_from_covidtracking_csv():
+    source_name = 'covid_sites_from_covidtracking_gsheet'
+    csv_path = 'domain_sources/{}.csv'.format(source_name)
+
+    all_possible_columns = ['COVID-19 site (OLD)', 'COVID-19 site', 'COVID-19 site (secondary)',
+        'COVID-19 site (tertiary)', 'COVID-19 site (quaternary)', 'COVID-19 site (quinary)'
+    ]
+    domains = set()
+    with open(csv_path) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            for col in all_possible_columns:
+                full_url = row[col]
+                if full_url is None:
+                    print('Skipping column {}'.format(col))
+                    continue
+                hostname = urlparse(full_url).hostname
+                if hostname is not None:
+                    domains.add(hostname)
+
+    return [{'source': source_name, 'domain': d} for d in domains]
+
+
 def write_all_domains(all_domains):
     with open('all_domains_to_check.csv', 'w') as f:
         writer = csv.DictWriter(f, fieldnames=all_domains[0].keys())
@@ -38,6 +62,9 @@ def main():
     all_domains.extend(read_from_csv('non_dotgov_gov_urls.csv', '\ufeffDomain'))
     all_domains.extend(read_from_csv('state_local_public_health.csv', 'domain'))
     all_domains.extend(read_from_csv('fed_gov_from_usa_dot_gov.csv', 'domain'))
+    # sites from covidtracking.com include a number of possible columns, special-case these
+    all_domains.extend(read_from_covidtracking_csv())
+
     write_all_domains(all_domains)
 
 if __name__ == '__main__':
