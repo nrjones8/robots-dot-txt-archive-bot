d6eb7fb56d26c98b84faeec3358d00bdab09dc8b
Nick Jones <nrjones8@gmail.com>
2020-10-17 16:18:27 -0400

    add stripped down version of create-nextjs-app

---
+++ robots-txt-webapp/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Hellllllllo from <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
