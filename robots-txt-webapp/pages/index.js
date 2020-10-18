ed1bec5ae291eb6b12b744cb7a7332c0736812a5
Nick Jones <nrjones8@gmail.com>
2020-10-18 19:13:15 -0400

    functioning but not particularly aesthetically-pleasing search, table display

--- robots-txt-webapp/pages/index.js
+++ robots-txt-webapp/pages/index.js
@@ -1,32 +1,85 @@
 import Head from 'next/head'
-import { Button, Col, Input, Layout, Row } from 'antd';
+import React, { Component } from 'react';
+import { Col, Input, Layout, Row, Table } from 'antd';
 
 const { Header, Footer, Content } = Layout;
+const { Search } = Input;
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchColumns: [
+        {
+          title: 'Site',
+          dataIndex: 'domain',
+        },
+        {
+          title: 'User Agent',
+          dataIndex: 'user_agent',
+        },
+        {
+          title: 'Rule',
+          dataIndex: 'rule',
+        },
+      ],
+      searchText: null,
+      searchResults: null,
+    };
 
-      <main>
-        <Header>
-          <h2 style={{ color: 'grey' }}>robots are fun</h2>
-        </Header>
-        <Content>
-          <Row>
-            <Col span={8}></Col>
-            <Col span={8}>
-              <Input placeholder="Search across a bunch of robots.txt"></Input>
-            </Col>
-            <Col span={8}></Col>
-          </Row>
-        </Content>
-        {/* <Footer>do i need a footer</Footer> */}
+    this.handleSearch = this.handleSearch.bind(this);
+  }
 
-      </main>
-    </div>
-  )
+  handleSearch(value, _) {
+    const fullUrl = `https://robots-dot-txt-db.herokuapp.com/robotstxt.json?_shape=objects&sql=select+*+from+all_parsed_robots_txt_data+where+rule+like+%27%25${value}%25%27+order+by+domain`
+    fetch(fullUrl)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          searchResults: data.rows
+        });
+      })
+      .catch((error) => {
+        console.log('got an error');
+        console.log(error);
+      })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+          <Header>
+            <h2 style={{ color: 'grey' }}>robots are fun</h2>
+          </Header>
+          <Content>
+            <Row>
+              <Col span={8}></Col>
+              <Col span={8}>
+                <Search
+                  placeholder="Search across a bunch of robots.txt"
+                  onSearch={this.handleSearch}
+                />
+              </Col>
+              <Col span={8}></Col>
+            </Row>
+            <Row>
+              <Col span={4} />
+              <Col span={16}>
+                <Table dataSource={this.state.searchResults} columns={this.state.searchColumns} />
+              </Col>
+              <Col span={4} />
+            </Row>
+          </Content>
+          {/* <Footer>do i need a footer</Footer> */}
+
+        </main>
+      </div>
+    )
+  }
 }
