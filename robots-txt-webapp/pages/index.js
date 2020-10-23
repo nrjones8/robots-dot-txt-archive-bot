2a42ae91be68d99d7dae6072eba42d95623bfd4f
Nick Jones <nrjones8@gmail.com>
2020-10-23 10:39:55 -0400

    include links in table display

--- robots-txt-webapp/pages/index.js
+++ robots-txt-webapp/pages/index.js
@@ -22,6 +22,17 @@ export default class Home extends Component {
           title: 'Rule',
           dataIndex: 'rule',
         },
+        {
+          title: 'Links',
+          dataIndex: 'links',
+          render: (linkObj, record) => (
+            <div>
+              {/* TODO - add external link icons */}
+              <p><a target="_blank" href={linkObj.liveLink}>Live version</a></p>
+              <p><a target="_blank" href={linkObj.archiveLink}>Wayback version</a></p>
+            </div>
+          )
+        },
       ],
       searchText: null,
       searchResults: null,
@@ -31,12 +42,33 @@ export default class Home extends Component {
   }
 
   handleSearch(value, _) {
+    // Datasette is read-only (and everyone has access to all the data, by design), so no
+    // need to do any extra SQL injection escaping here. See
+    // https://docs.datasette.io/en/stable/sql_queries.html?highlight=injection#running-sql-queries
     const fullUrl = `https://robots-dot-txt-db.herokuapp.com/robotstxt.json?_shape=objects&sql=select+*+from+all_parsed_robots_txt_data+where+rule+like+%27%25${value}%25%27+order+by+domain`
     fetch(fullUrl)
       .then(response => response.json())
       .then((data) => {
+        // each row is an object like:
+        // domain: "accesskent.com"
+        // domain_source: "non_dotgov_gov_urls"
+        // rule: "Disallow: /DogLicense/"
+        // user_agent: "User-agent: *"
+
+        const rowsWithLinks = data.rows.map(row => {
+          let liveLink = '';
+          let archiveLink = '';
+          if (row.rule.includes('allow')) {
+            const splitRule = row.rule.split(':');
+            const url = splitRule[1].trim();
+            liveLink = `https://${row.domain}${url}`;
+            archiveLink = `https://web.archive.org/web/*/${row.domain}${url}`;
+          }
+          return {...row, links: {liveLink, archiveLink} };
+        })
+
         this.setState({
-          searchResults: data.rows
+          searchResults: rowsWithLinks
         });
       })
       .catch((error) => {
@@ -69,11 +101,15 @@ export default class Home extends Component {
               <Col span={8}></Col>
             </Row>
             <Row>
-              <Col span={4} />
-              <Col span={16}>
-                <Table dataSource={this.state.searchResults} columns={this.state.searchColumns} />
+              <Col span={2} />
+              <Col span={20}>
+                <Table
+                  dataSource={this.state.searchResults}
+                  columns={this.state.searchColumns}
+                  pagination={ {defaultPageSize: 50} }
+                />
               </Col>
-              <Col span={4} />
+              <Col span={2} />
             </Row>
           </Content>
           {/* <Footer>do i need a footer</Footer> */}
